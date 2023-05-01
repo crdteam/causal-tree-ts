849c22b06bb657e57e2e848134307a0d53f008e9
samuelv8 <78799492+samuelv8@users.noreply.github.com>
2023-05-01 16:07:01 -0300

    feat: add weave traversal functions

--- src/utils/functions.ts
+++ src/utils/functions.ts
@@ -1,3 +1,25 @@
 import { v1 as uuidv1 } from 'uuid';
+import type Atom from '../Atom';
 
 export const getNewUuid = (): string => uuidv1();
+
+export const causalBlockLenght = (block: Atom[]): number => {
+  if (block.length === 0) return 0;
+  const [head, ...tail] = block;
+  const endIndex = tail.findIndex((atom) => atom.cause.timestamp < head.id.timestamp);
+  return endIndex === -1 ? block.length : endIndex + 1;
+};
+
+export const walkCausalBlock = (block: Atom[], callback: (atom: Atom) => boolean): number => {
+  const endIndex = causalBlockLenght(block);
+  const causalBlock = block.slice(0, endIndex);
+  return causalBlock.findIndex((atom) => !callback(atom));
+};
+
+export const walkChildren = (block: Atom[], callback: (atom: Atom) => boolean): number => {
+  const [head] = block;
+  return walkCausalBlock(block, (atom) => {
+    if (atom.cause === head.id) return callback(atom);
+    return true;
+  });
+};
