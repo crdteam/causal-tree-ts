236f2032214bfbb9ce3785c8e738b6d9a41e7a50
samuelv8 <78799492+samuelv8@users.noreply.github.com>
2023-07-13 23:51:53 -0300

    fix(Value): snapshot return type

--- src/api/Value.d.ts
+++ src/api/Value.d.ts
@@ -3,10 +3,10 @@ import TreePosition from '../core/TreePosition';
 /**
  * Value represents a structure that may be converted to concrete data.
  * @property treePosition - The position of the value in the tree.
- * @property snapshot - A function that returns a string representation of the
- * operation.
+ * @property snapshot - A function that returns a representation of the
+ * pointed operation.
  */
 export interface Value {
   treePosition: TreePosition;
-  snapshot(): string;
+  snapshot(): any;
 }
